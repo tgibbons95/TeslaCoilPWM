1011346fbee02ba527f93f990fe51ed09522209d
Thomas Gibbons <gibbons.thomas95@gmail.com>
2018-04-04 21:14:51 -0500

    Toggled with bootup sequence

--- MainControl.cpp
+++ MainControl.cpp
@@ -4,6 +4,6 @@ int main(void){
 	system("sudo insmod /home/pi/ECE4970/Kernel/Kernel.ko");
 	system("sudo mknod /dev/TeslaCoil c 244 0");
 	system("sudo chmod go+w /dev/TeslaCoil");
-	system("cd /home/pi/ECE4970/ && ./main");
+	system("/home/pi/ECE4970/main");
 	return 0;	
 }
